f97e8bd3808f2fdaef0a83a0fc5ae81e77fd779b
nazas1224 <54030731+nazas1224@users.noreply.github.com>
2019-08-13 13:29:39 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,100 @@
+variable "auth_token" {
+  description="Your Cherryservers API token"
+#  default ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJjbGllbnRfaWQiOjM1ODk0LCJpYXQiOjE1NjM5NTIyMDl9.da4ZwnuWqMUnEe7cry_ID64sjTmMv20BL6UH1cHrMtA"
+}
+
+variable "team_id" {
+  description = "Your Team ID from portal.cherryservers.com"
+#  default = ""
+}
+
+resource "cherryservers_ssh" "kontena_ssh" {
+  name       = "kontena_ssh"
+  public_key = "${file("~/.ssh/Your_ssh.key")}"
+}
+
+variable "cluster_name" {
+  default = "pharos"
+}
+
+variable "region" {
+  default = "EU-East-1"
+}
+
+variable "master_plan" {
+  default = "113"
+}
+
+variable "worker_plan" {
+  default = "113"
+}
+
+variable "master_count" {
+  default = 1
+}
+
+variable "worker_count" {
+  default = 3
+}
+
+variable "host_os" {
+  default = "CentOS 7 64bit"
+}
+
+provider "cherryservers" {
+    auth_token = "${var.auth_token}"
+}
+
+resource "cherryservers_project" "Kontena_Pharos" {
+  team_id = "${var.team_id}"
+  name    = "Kontena_Pharos"
+}
+resource "cherryservers_ip" "floating-ip-kontena-worker" {
+    project_id = "${cherryservers_project.Kontena_Pharos.id}"
+    region = "${var.region}"
+    count = "${var.worker_count}"
+}
+
+resource "cherryservers_server" "pharos_master" {
+  count           = "${var.master_count}"
+  hostname        = "${var.cluster_name}-master-${count.index}"
+  plan_id         = "${var.master_plan}"
+  region          = "${var.region}"
+  image           = "${var.host_os}"
+  project_id      = "${cherryservers_project.Kontena_Pharos.id}"
+  ssh_keys_ids    = ["${cherryservers_ssh.kontena_ssh.id}"]
+  }
+
+resource "cherryservers_server" "pharos_worker" {
+  count        = "${var.worker_count}"
+  hostname     = "${var.cluster_name}-worker-${count.index}"
+  plan_id      = "${var.worker_plan}"
+  region       = "${var.region}"
+  image        = "${var.host_os}"
+  project_id   = "${cherryservers_project.Kontena_Pharos.id}"
+  ssh_keys_ids = ["${cherryservers_ssh.kontena_ssh.id}"]
+  ip_addresses_ids = ["${cherryservers_ip.floating-ip-kontena-worker.*.id[count.index]}"]
+}
+
+output "pharos_hosts" {
+  value = {
+    masters = {
+      address         = "${cherryservers_server.pharos_master.*.primary_ip}"
+      private_address = "${cherryservers_server.pharos_master.*.private_ip}"
+      role            = "master"
+      user            = "root"
+    }
+    workers = {
+      address         = "${cherryservers_server.pharos_worker.*.primary_ip}"
+      private_address = "${cherryservers_server.pharos_worker.*.private_ip}"
+      role            = "worker"
+      user            = "root"
+      label = {
+        ingress = "nginx"
+
+      }
+    }
+  }
+}
+
+
